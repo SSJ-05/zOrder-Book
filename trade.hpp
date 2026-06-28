4220a0b01af9c9871382ceb2a7cba54c6d362184
ZeroK_Tech <149679147+SSJ-05@users.noreply.github.com>
2026-06-28 14:56:54 +0530

    Add files via upload

--- trade.hpp
+++ trade.hpp
@@ -9,6 +9,8 @@ struct Trade {
     std::uint64_t   buy_id;
     std::uint64_t   sell_id;
 
-    double          price;
+    double          price;      // replace
     std::uint32_t   qty;
 };
+
+static_assert (std::is_trivially_copyable_v<Trade>);
