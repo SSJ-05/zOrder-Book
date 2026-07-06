09d1a490c6e3526c2a29d915f2fe319673fb2d0b
ZeroK_Tech <149679147+SSJ-05@users.noreply.github.com>
2026-07-06 21:41:27 +0530

    Add files via upload

--- LOBv4/intrusive_list.cpp
+++ LOBv4/intrusive_list.cpp
@@ -9,52 +9,80 @@
 #include "intrusive_list.hpp"
 #include "order.hpp"
 
+#include <cassert>
+
 
 void IntrusiveList::push_back (Order* order) {
 
-    if (empty()) {
+    assert (order != nullptr);
+    assert (!order->inlist);    // prevent double insertion
 
+    order->prev = nullptr;      // make sure new node... 
+    order->next = nullptr;      // ...doesnt point to anything
+
+    if (empty()) {
+        head_ = order;
+        tail_ = order;
+
+        order->inlist = true;
     }
     else {
+        tail_->next = order;
+        order->prev = tail_;
+        tail_ = order;          // new tail
+                                
+        order->inlist = true;
     }
 }
 
+
 // 5 cases
 void IntrusiveList::erase (Order* order) {
+
+    assert (order != nullptr);
+    assert (order->inlist);     // must be in the list to erase
 
     // case 1: empty list
     if (empty()) return;
 
     // case 2: one node
-    else if (head_ == tail_) {
+    if (head_ == tail_) {
+        // rewire
         head_ = nullptr;
         tail_ = nullptr;
-        return;
     } 
 
     // case 3: head
-    else if (order == head_) {
+    if (order == head_) {
+        // rewire
         head_ = head_->next;
         head_->prev = nullptr;
-        return;
     }
 
     // case 4: tail
-    else if (order == tail_) {
+    if (order == tail_) {
+        // rewire
         tail_ = tail_->prev;
         tail_->next = nullptr;
-        return;
     }
-
 
     // case 5: middle node
     else  {
+        // rewire
         order->prev->next = order->next;
         order->next->prev = order->prev;
-        return;
     }
 
+    // cleanup
+    order->prev   = nullptr;  // make sure removed node... 
+    order->next   = nullptr;  // ...doesnt point to anything
+    order->inlist = false;    // mark as removed 
+
+    return;
 }
+
+assert (head_ == nullptr || head_->prev == nullptr);
+assert (tail_ == nullptr || tail_->next == nullptr);
 
 
 
