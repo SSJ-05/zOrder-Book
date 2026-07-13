df6fd45f8d6a24c4b0017b650b73bff30afa2408
ZeroK_Tech <149679147+SSJ-05@users.noreply.github.com>
2026-07-13 22:34:57 +0530

    Add files via upload

---
+++ ring_price_ladder.hpp
@@ -0,0 +1,53 @@
+// ring price ladder header file// 08.07.26// ZeroK
+
+#pragma once
+
+#include "orderv2.hpp"
+#include "price_level.hpp"
+#include "types.hpp"
+
+#include <array>
+#include <cstdint>
+
+
+
+class RingPriceLadder {
+
+private:
+    static constexpr std::size_t NUM_LEVELS_  { 1 << 12 };
+    static constexpr std::size_t MASK_        { NUM_LEVELS_ - 1 };
+    static constexpr std::size_t INVALID_     { NUM_LEVELS_ };
+
+    Price  best_idx_    {};
+    Price  base_price_  {};
+    Side   side_        { Bid };
+
+    std::array<PriceLevel, NUM_LEVELS_> rpl_ {};
+
+
+public:
+    // ctor
+    explicit RingPriceLadder (Price base, Side side) :
+        base_price_ (base),
+        side_ (side) {}
+
+
+    // ops
+    void  add          (Order*) noexcept;
+    void  remove       (Order*) noexcept;
+    void  clear_level  (Price)  noexcept;
+
+    void  update_best_after_add    (Price) noexcept;
+    void  update_best_after_remove (Price) noexcept;
+
+    std::size_t  to_idx   (Price) const noexcept;
+    bool         contains (Price) const noexcept;
+
+    PriceLevel&  at_level (Price) noexcept;
+    PriceLevel*  best_level()     noexcept;
+
+   
+    // reserved for future versions
+    void advance_window (Price new_base);
+};
+
