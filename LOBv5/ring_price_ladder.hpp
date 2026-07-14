17859e6200e93de2f62acb23491313652df1a7ba
ZeroK_Tech <149679147+SSJ-05@users.noreply.github.com>
2026-07-14 21:46:42 +0530

    Add files via upload

--- LOBv5/ring_price_ladder.hpp
+++ LOBv5/ring_price_ladder.hpp
@@ -18,9 +18,9 @@ private:
     static constexpr std::size_t MASK_        { NUM_LEVELS_ - 1 };
     static constexpr std::size_t INVALID_     { NUM_LEVELS_ };
 
-    Price  best_idx_    {};
-    Price  base_price_  {};
-    Side   side_        { Bid };
+    Price        base_price_  {};
+    Side         side_        { Side::Bid };
+    std::size_t  best_idx_    { INVALID_ };
 
     std::array<PriceLevel, NUM_LEVELS_> rpl_ {};
 
@@ -33,22 +33,20 @@ public:
 
 
     // ops
-    void add (Order*) noexcept;
+    void  add          (Order*) noexcept;
+    void  remove       (Order*) noexcept;
+    void  clear_level  (Price)  noexcept;
 
-    void remove (Order*) noexcept;
+    void  update_best_after_add    (Price) noexcept;
+    void  update_best_after_remove (Price) noexcept;
 
-    void clear_level (Price) noexcept;
+    std::size_t  to_idx   (Price) const noexcept;
+    bool         contains (Price) const noexcept;
 
-    void update_best_after_add    (Price) noexcept;
-    void update_best_after_remove (Price) noexcept;
-
-    std::size_t to_idx (Price) const noexcept;
-
-    bool contains (Price) const noexcept;
-
-    PriceLevel& at_level (Price) noexcept;
-
-    PriceLevel* best_level();
+    const PriceLevel&  at_level (Price) const noexcept;
+          PriceLevel&  at_level (Price) noexcept;
+    
+    PriceLevel*  best_level()  noexcept;
 
    
     // reserved for future versions
